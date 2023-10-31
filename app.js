7cbca6d9955b57133499b9b4ab55258a4193aacc
Exeki <d.exeki@gmail.com>
2023-10-31 16:49:19 -0300

    integración de index y css

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) =>  {
-    console.log('peticion recibida')
-    res.send('HOLA con el script DEV y Erwin, se modifica el gitignore')
-})
+app.use(express.static(path.join(__dirname,'public')))
+
+//app.get('/', (req, res) =>  {
+    //console.log('peticion recibida')
+    //res.status(200).sendFile('index.html',{root:__dirname})
+//})
 
 const PORT = process.env.PORT
 
