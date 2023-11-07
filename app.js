6a65be2370f41fd0708622fa5d42802e14347651
Exeki <d.exeki@gmail.com>
2023-11-07 14:44:29 -0300

    HTML textbox y conectividad BBDD

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const path = require('path')
 require('dotenv').config()
+
+const app = express()
 
 mongoose
     .connect(
       `mongodb+srv://exekin:${process.env.MONGO_DB_PASS}@micluster.bbvmrmb.mongodb.net/?retryWrites=true&w=majority`
 )
-.then((result) => console.log('Conexión exitosa a la Base de datos'))
+.then((result) =>{
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+    console.log('Conexión existosa a la Base de Datos')
+})
 .catch((err) => console.log('error'))
 
-const app = express()
+// console.log('Hola a todos')
 
-// app.get('/', (req, res, next) =>  {
-//     console.log('peticion recibida')
-//     next()
-// })
+app.use(express.json())
+
+app.post('/api/v1/products', (req, res) =>  {
+    console.log('peticion recibida')
+    console.log({body: req.body});
+    res.status(201).json({ok: true})
+})
 
 app.use(express.static(path.join(__dirname,'public')))
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
+
--- public/index.js
+++ public/index.js
@@ -1,3 +1,23 @@
-const h1 = document.querySelector('h1')
+const inputName = document.querySelector('#productName')
+const inputPrice = document.querySelector('#productPrice')
 
-console.log(h1)
+const button = document.querySelector('button')
+
+button.addEventListener('click',(e) =>{
+    const name = inputName.value
+    const price = inputPrice.value
+
+
+fetch('/api/v1/products',{
+    method: 'POST', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        name,
+        price,
+    }),
+})
+
+})
+
