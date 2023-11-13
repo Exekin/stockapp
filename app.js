934b1e16b5078d5414911b0f33b0f9e055058659
Exeki <d.exeki@gmail.com>
2023-11-13 15:39:52 -0300

    Actualización MONGODB,HTML,CSS,JS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { timeStamp } = require('console')
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
@@ -7,7 +8,7 @@ const app = express()
 
 mongoose
     .connect(
-      `mongodb+srv://exekin:${process.env.MONGO_DB_PASS}@micluster.bbvmrmb.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://exekin:${process.env.MONGO_DB_PASS}@micluster.bbvmrmb.mongodb.net/stock-app?retryWrites=true&w=majority`
 )
 .then((result) =>{
 app.listen(PORT, () => {
@@ -15,16 +16,33 @@ app.listen(PORT, () => {
 })
     console.log('Conexión existosa a la Base de Datos')
 })
-.catch((err) => console.log('error'))
+    .catch((err) => console.log('error'))
 
 // console.log('Hola a todos')
 
+const productsSchema = mongoose.Schema(
+    {
+    name: { type: String, require: true},
+    price: Number,
+    },
+    { timestamps: true }
+
+)
+
+const Product = mongoose.model('Product', productsSchema)
+
 app.use(express.json())
 
-app.post('/api/v1/products', (req, res) =>  {
-    console.log('peticion recibida')
-    console.log({body: req.body});
-    res.status(201).json({ok: true})
+app.post('/api/v1/products', (req, res) =>  
+    {
+    const newProduct = new Product(req.body)
+
+    newProduct
+    .save()
+    .then((result)=>{
+      res.status(201).json({ok: true})
+    })
+    .catch((err) => console.log(err))
 })
 
 app.use(express.static(path.join(__dirname,'public')))
